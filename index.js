44412c8206722537ddf4e9cbe5f5b3b239848039
Godwin Clayd Enriquez <95603-18526902@SERVER2.DOMAIN>
2024-08-19 18:21:32 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const user = {
+	fname: "Jessica",
+	lname: "Cordero",
+	gender: "Female",
+	age: 30
+}
+
+
+console.log("I am " + user.fname + " "+ user.lname + " and I am "+ user.age +" years old")
